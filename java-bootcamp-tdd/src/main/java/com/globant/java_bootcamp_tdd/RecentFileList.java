f36ad47e5a405f4600275a19aea89335e9bf0dbd
LUCAS . <YO_ILCAPO@hotmail.com>
2018-07-18 01:37:45 -0300

    TDD practice 1

---
+++ java-bootcamp-tdd/src/main/java/com/globant/java_bootcamp_tdd/RecentFileList.java
@@ -0,0 +1,52 @@
+package com.globant.java_bootcamp_tdd;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class RecentFileList {
+
+	private ArrayList<File> list;
+	private int maxSize = 15;
+
+	public RecentFileList() {
+		this.list = new ArrayList<File>();
+	}
+
+	public RecentFileList(int size) {
+		this.list = new ArrayList<File>();
+		this.maxSize = size;
+	}
+
+	public int size() {
+		return list.size();
+	}
+
+	public void openFile(File file) {
+		if (list.size() >= maxSize) {
+			list.remove(0);
+		}
+		if (!list.contains(file)) {
+			list.add(file);
+		} else {
+			list.remove(file);
+			list.add(file);
+		}
+	}
+
+	public boolean exist(File file) {
+		return list.contains(file);
+	}
+
+	public int positionOfFile(File file) {
+		// return the position of 1 file in the recent opened files, or -1 if not exist
+		// in this.
+		if (this.exist(file)) {
+			return list.indexOf(file);
+		}
+		return -1;
+	}
+
+	public File lastElemment() {
+		return list.get(list.size() - 1);
+	}
+}
---
+++ java-bootcamp-tdd/src/test/java/com/globant/java_bootcamp_tdd/RecentFileListTest.java
@@ -0,0 +1,57 @@
+package com.globant.java_bootcamp_tdd;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class RecentFileListTest {
+
+	// For this tests the list of recent opened files have a size of 5, the
+	// predetermined(in the recent file list class) is 15
+	RecentFileList list = new RecentFileList(5);
+
+	@Test
+	public final void whenTheProgramIsRunForTheFirstTimeTheListIsEmpty() {
+		assertEquals(0, list.size());
+	}
+
+	@Test
+	public final void whenAFileIsOpenedItIsAddedToTheRecentFileList() {
+		File file = new File("t2");
+		list.openFile(file);
+		assert (list.exist(file));
+	}
+
+	@Test
+	public final void ifAnOpenedFileAlreadyExistsInTheRecentFileListItIsBumpedToTheTopNotDuplicatedInTheList() {
+		File file1 = new File("t3-1");
+		File file2 = new File("t3-2");
+		File file3 = new File("t3-3");
+		list.openFile(file1);
+		list.openFile(file2);
+		list.openFile(file3);
+		list.openFile(file1);
+		assertEquals(file1, list.lastElemment());
+	}
+
+	@Test
+	public final void ifTheRecentFileListGetsFullTheOldestItemIsRemovedWhenANewItemIsAdded() {
+		File file1 = new File("t4-1");
+		File file2 = new File("t4-2");
+		File file3 = new File("t4-3");
+		File file4 = new File("t4-4");
+		File file5 = new File("t4-5");
+		File file6 = new File("t4-6");
+		list.openFile(file1);
+		list.openFile(file2);
+		list.openFile(file3);
+		list.openFile(file4);
+		list.openFile(file5);
+		list.openFile(file6);
+		assert (!list.exist(file1));
+		assert (list.exist(file2));
+	}
+
+}
